c6333eaf811f335d9d741c60657d2bcb073d61e3
John Doe <n.kostrukov@mail.ru>
2025-03-17 15:56:28 +0300

    update ex7

--- src/ex2.sql
+++ src/ex2.sql
@@ -45,6 +45,33 @@ ORDER BY NS.Счет, VZ.Номер
 
 
 
+
+-- drop table if EXISTS #tempVZ
+
+-- SELECT VZ.Номер, VZ.[Счет-Взыскания], Sd.Сумма2 as 'Пени', SD.ГосПошлина, VZ.ДатНачДолга, VZ.ДатКнцДолга,
+--  CONCAT(VF.Название, ' ', SF.Имя) as 'Фаза + Состояние', SD.НомерДокумента, SD.ROW_ID as 'ROW_ID_Состояния'
+-- into #tempVZ
+-- FROM [Взыскание задолженности] VZ
+-- JOIN [Состояние документа] SD ON SD.[Документ-Состояние] = VZ.[ROW_ID]
+-- JOIN [Виды фаз] VF ON VF.ROW_ID = SD.[Фаза-Состояние]
+-- LEFT JOIN [Состояния фаз] SF ON SD.[ПодФаза-Состояние] = SF.ROW_ID
+-- JOIN [Держатели долга] DD ON DD.ROW_ID = VZ.[Держатели долга-Взыскания]
+-- JOIN [Организации] O ON O.ROW_ID = DD.[Держатели-Организация]
+-- JOIN Производство P ON P.[Производство-Дело] = VZ.ROW_ID
+-- WHERE  O.ИНН ='3444259579'
+-- AND O.ROW_ID IN (SELECT KO.[Категории-Организация] FROM [Категории организаций] KO WHERE KO.[Категории организаций] IN (7036, 7034))
+-- GROUP BY VZ.Номер, VZ.[Счет-Взыскания],  SD.ГосПошлина, Sd.Сумма2, VZ.ДатНачДолга, VZ.ДатКнцДолга, VF.Название, SF.Имя, SD.НомерДокумента, SD.ROW_ID
+
+
+-- SELECT VZ.Номер, VZ.[Фаза + Состояние], VZ.НомерДокумента, ROUND(SUM(NS.Сумма), 2) as 'Сумма', VZ.Пени, VZ.ГосПошлина
+-- FROM #tempVZ VZ
+-- JOIN #tempNS NS ON NS.Счет = VZ.[Счет-Взыскания]
+-- WHERE [Месяц долга] BETWEEN Vz.ДатНачДолга AND VZ.ДатКнцДолга AND VZ.Номер = 2148408561
+-- GROUP BY VZ.Номер, VZ.НомерДокумента, VZ.[Фаза + Состояние], VZ.[ROW_ID_Состояния], VZ.Пени, VZ.ГосПошлина
+-- ORDER BY VZ.Номер
+
+
+
 drop table if EXISTS #tempNS
 
 SELECT ROUND(SUM(NS.[Сумма]), 2) as "Сумма", LS.Номер as 'Номер ЛЦ', NS.Счет, NS.[Месяц долга], O.Наименование, O.ROW_ID as 'ROW_ID_Организации', 
@@ -56,35 +83,8 @@ JOIN [Организации] O ON O.ROW_ID = NS.Поставщик
 JOIN [Карточки регистрации] KR ON KR.ROW_ID = NS.Счет
 WHERE NS.[Месяц расчета] = '2025-02-01' AND NS.[Месяц долга] < '2025-02-01' and NS.[Сумма] > 0
 AND O.ИНН ='3444259579'
-AND O.ROW_ID IN (SELECT KO.[Категории-Организация] FROM [Категории организаций] KO WHERE KO.[Категории организаций] IN (7036, 7034))
+-- AND O.ROW_ID IN (SELECT KO.[Категории-Организация] FROM [Категории организаций] KO WHERE KO.[Категории организаций] IN (7036, 7034))
 GROUP BY LS.Номер, NS.Счет, NS.[Месяц долга], O.Наименование, O.ROW_ID, KR.ФИО, KR.[Дата рождения], KR.[Дата смерти]
-
-drop table if EXISTS #tempVZ
-
-SELECT VZ.Номер, VZ.[Счет-Взыскания], Sd.Сумма2 as 'Пени', SD.ГосПошлина, VZ.ДатНачДолга, VZ.ДатКнцДолга,
- CONCAT(VF.Название, ' ', SF.Имя) as 'Фаза + Состояние', SD.НомерДокумента, SD.ROW_ID as 'ROW_ID_Состояния'
-into #tempVZ
-FROM [Взыскание задолженности] VZ
-JOIN [Состояние документа] SD ON SD.[Документ-Состояние] = VZ.[ROW_ID]
-JOIN [Виды фаз] VF ON VF.ROW_ID = SD.[Фаза-Состояние]
-LEFT JOIN [Состояния фаз] SF ON SD.[ПодФаза-Состояние] = SF.ROW_ID
-JOIN [Держатели долга] DD ON DD.ROW_ID = VZ.[Держатели долга-Взыскания]
-JOIN [Организации] O ON O.ROW_ID = DD.[Держатели-Организация]
-JOIN Производство P ON P.[Производство-Дело] = VZ.ROW_ID
-WHERE  O.ИНН ='3444259579'
-AND O.ROW_ID IN (SELECT KO.[Категории-Организация] FROM [Категории организаций] KO WHERE KO.[Категории организаций] IN (7036, 7034))
-GROUP BY VZ.Номер, VZ.[Счет-Взыскания],  SD.ГосПошлина, Sd.Сумма2, VZ.ДатНачДолга, VZ.ДатКнцДолга, VF.Название, SF.Имя, SD.НомерДокумента, SD.ROW_ID
-
-
-SELECT VZ.Номер, VZ.[Фаза + Состояние], VZ.НомерДокумента, ROUND(SUM(NS.Сумма), 2) as 'Сумма', VZ.Пени, VZ.ГосПошлина
-FROM #tempVZ VZ
-JOIN #tempNS NS ON NS.Счет = VZ.[Счет-Взыскания]
-WHERE [Месяц долга] BETWEEN Vz.ДатНачДолга AND VZ.ДатКнцДолга AND VZ.Номер = 2148408561
-GROUP BY VZ.Номер, VZ.НомерДокумента, VZ.[Фаза + Состояние], VZ.[ROW_ID_Состояния], VZ.Пени, VZ.ГосПошлина
-ORDER BY VZ.Номер
-
-
-
 
 
 drop table if EXISTS #tempVZ
@@ -98,9 +98,8 @@ JOIN [Виды фаз] VF ON VF.ROW_ID = SD.[Фаза-Состояние]
 LEFT JOIN [Состояния фаз] SF ON SD.[ПодФаза-Состояние] = SF.ROW_ID
 JOIN [Держатели долга] DD ON DD.ROW_ID = VZ.[Держатели долга-Взыскания]
 JOIN [Организации] O ON O.ROW_ID = DD.[Держатели-Организация]
-JOIN Производство P ON P.[Производство-Дело] = VZ.ROW_ID
-WHERE  O.ИНН ='3444259579'
-AND O.ROW_ID IN (SELECT KO.[Категории-Организация] FROM [Категории организаций] KO WHERE KO.[Категории организаций] IN (7036, 7034))
+WHERE  O.ИНН ='3444259579' and SD.НомерДокумента = '2-89-703/2022'
+-- AND O.ROW_ID IN (SELECT KO.[Категории-Организация] FROM [Категории организаций] KO WHERE KO.[Категории организаций] IN (7036, 7034))
 GROUP BY VZ.Номер, VZ.[Счет-Взыскания],  SD.ГосПошлина, Sd.Сумма2, VZ.ДатНачДолга, VZ.ДатКнцДолга, VF.Название, SF.Имя, SD.НомерДокумента, SD.ROW_ID, VZ.ОплатаПени, VZ.ОплатаГоспошлины
 
 drop table if EXISTS #tempSP
@@ -110,12 +109,15 @@ SELECT VZ.Номер, VZ.Фаза, VZ.НомерДокумента, NS.Счет,
     CASE VZ.Фаза WHEN 'Подача' THEN ROUND(VZ.ГосПошлина - VZ.ОплатаГоспошлины, 2) ELSE ROUND(VZ.ГосПошлина, 2) END as 'ГосПошлина', VZ.ОплатаПени, VZ.ОплатаГоспошлины
 into #tempSP
 FROM #tempVZ VZ
-JOIN #tempNS NS ON NS.Счет = VZ.[Счет-Взыскания]
-WHERE [Месяц долга] BETWEEN Vz.ДатНачДолга AND VZ.ДатКнцДолга 
+LEFT JOIN #tempNS NS ON NS.Счет = VZ.[Счет-Взыскания] --and [Месяц долга] BETWEEN ДатНачДолга AND ДатКнцДолга
+-- WHERE VZ.НомерДокумента = '2-89-254/2023'
 GROUP BY VZ.Номер, VZ.НомерДокумента, VZ.Фаза, VZ.[ROW_ID_Состояния], VZ.Пени, VZ.ГосПошлина, VZ.ОплатаПени, VZ.ОплатаГоспошлины, NS.Счет
 ORDER BY VZ.Номер
 
-SELECT distinct SP.НомерДокумента, SP.Номер, Sp.Счет, SP.Сумма, SP.Пени, SP.ГосПошлина
+
+SELECT SP.НомерДокумента as 'Номер ИД', SP.Номер, Sp.Счет, SP.Сумма, SP.Пени, SP.ГосПошлина
 FROM #tempSP SP
-WHERE SP.НомерДокумента != ''
+WHERE SP.НомерДокумента != '' --and SP.НомерДокумента = '2-88-3397/2022'
 ORDER BY SP.НомерДокумента
+
+SELECT * FROM #tempSP
